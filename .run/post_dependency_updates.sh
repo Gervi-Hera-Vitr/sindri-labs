21b7093c05d2ea51a4b37e1620aba7270ba8763f
rdd13r <12781006+rdd13r@users.noreply.github.com>
2024-10-28 07:09:51 -0400

    Testing issue access.

--- .run/post_dependency_updates.sh
+++ .run/post_dependency_updates.sh
@@ -29,10 +29,25 @@ else
   echo "No dependency updates available." > dependency-updates.md
 fi
 
-if [ -z "${ISSUE_NUMBER}" ]; then
+if [ -z "${ISSUE_NUMBER+x}" ]; then
     echo -e "\nPlease bind an issue to commit to receive version recommendations.\n\n"
-    cat dependency-updates.md
 else
     echo -e "\nWorking on issue #${ISSUE_NUMBER}\n\n"
     gh issue comment "$ISSUE_NUMBER" --body "$(cat dependency-updates.md)"
 fi
+
+
+echo -e "\n\nDependency Update Suggestions:"
+tee dependency-updates.md
+echo -e "\n\n"
+
+tee << EOF
+
+        Environment:
+
+          Event:           ${GH_EVENT}
+          Token:           ${GITHUB_TOKEN}
+          Issue:           ${ISSUE_NUM}
+          PR:              ${PR_NUM}
+
+EOF
