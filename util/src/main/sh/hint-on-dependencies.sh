cf9f91afb5310de1d42e257ad3ba03b216e5cad0
rdd13r <12781006+rdd13r@users.noreply.github.com>
2024-12-06 23:17:37 -0500

    Correcting dependency hints summary.

--- util/src/main/sh/hint-on-dependencies.sh
+++ util/src/main/sh/hint-on-dependencies.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env zsh
 
+step_summary_file="$GITHUB_STEP_SUMMARY"
+
 tick="$(date +%Y_%m_%d_%H_%M_%S)"
 build_dependencies_working_directory=$(git rev-parse --show-toplevel)/build/dependencies
 
 production_run=${1:-true}
 [[ "Darwin" == "$(uname)" ]] && production_run=false && echo "::notice file=hint-on-dependencies.sh,line=7::Running in development mode. Skipping prune actions."
-echo -e "==> production_run=$production_run\n\n"
 
 [[ ! -d "$build_dependencies_working_directory" ]] && mkdir -p "$build_dependencies_working_directory"
 
-if [[ "$production_run" != "true" ]]; then
-  GITHUB_STEP_SUMMARY="$build_dependencies_working_directory/github_step_summary_local_$tick.md"
-  touch "$GITHUB_STEP_SUMMARY"
-  echo -e "==> Running in development mode. \nReport file is written to file://$GITHUB_STEP_SUMMARY\n\n"
+if [[ "$production_run" == "false" ]]; then
+  step_summary_file="$build_dependencies_working_directory/github_step_summary_local_$tick.md"
+  touch "$step_summary_file"
+  echo -e "==> Running in development mode."
 fi
+
+echo -e "==> production_run =$production_run\n\n"
+echo -e "==> step_summary   =$step_summary_file\n\n"
 
 # File paths
 gradle_log_file="$build_dependencies_working_directory/gradle_dependencies_$tick.log"
@@ -36,7 +40,7 @@ else
       echo
       echo "_Please reach out to the [Gervi Héra Vitr](https://github.com/Gervi-Hera-Vitr) organization members for more information._"
       echo
-    }  >> "$GITHUB_STEP_SUMMARY"
+    }  >> "$summary_file_md"
     else
       echo "==> Run sdk c locally for SDKMAN state."
   fi
@@ -50,7 +54,7 @@ if ! command -v gradle &> /dev/null; then
     echo
     echo "_Gradle command not found on $(hostname) - $(whoami) - $tick. Terminating!_"
     echo
-  }  >> "$GITHUB_STEP_SUMMARY"
+  }  >> "$summary_file_md"
   [[ "$production_run" == "true" ]] && exit 1
   exit 0
 fi
@@ -65,7 +69,7 @@ if ! gradle --info --build-cache --no-daemon processDependencyUpdates > "$gradle
     echo
     echo "_Please reach out to the [Gervi Héra Vitr](https://github.com/Gervi-Hera-Vitr) organization members for more information._"
     echo
-  }  >> "$GITHUB_STEP_SUMMARY"
+  }  >> "$summary_file_md"
   [[ "$production_run" == "true" ]] && exit 1
   exit 0
 fi
@@ -81,7 +85,7 @@ if [[ ! -f "$report_file" ]]; then
       echo
       echo "_Please reach out to the [Gervi Héra Vitr](https://github.com/Gervi-Hera-Vitr) organization members for more information._"
       echo
-    }  >> "$GITHUB_STEP_SUMMARY"
+    }  >> "$summary_file_md"
     exit 1
 fi
 
@@ -131,7 +135,7 @@ echo "::notice file=hint-on-dependencies.sh,line=92::Total count: $total_count; 
     if [[ "$unresolved_count" -eq 0 ]]; then
         echo "- No unresolved dependencies."
     else
-        jq -r '.unresolved.dependencies[] | "- \(.group):\(.name)"' "$report_file"
+        echo "- $unresolved_count dependencies could not be resolved."
     fi
 } > "$summary_file_md"
 
@@ -139,13 +143,10 @@ echo "::notice file=hint-on-dependencies.sh,line=92::Total count: $total_count; 
 jq '{
   current: { count: .current.count, dependencies: .current.dependencies },
   outdated: { count: .outdated.count, dependencies: .outdated.dependencies },
-  undeclared: { count: .undeclared.count, dependencies: .undeclared.dependencies },
-  unresolved: { count: .unresolved.count, dependencies: .unresolved.dependencies }
+  undeclared: { count: .undeclared.count, dependencies: .undeclared.dependencies }
 }' "$report_file" > "$summary_file_json"
 
-echo "::notice file=hint-on-dependencies.sh,line=146::$(cat "$summary_file_json")"
-
-echo "Dependency summary (JSON) generated at $summary_file_json."
+echo "::notice file=hint-on-dependencies.sh,line=146::Dependency summary (JSON) generated at $summary_file_json."
 
 if [[ ! "$production_run" == "true" ]]; then
   echo -e "\n=== Summary ==="
@@ -154,3 +155,5 @@ if [[ ! "$production_run" == "true" ]]; then
   open "$report_file"
   open "$summary_file_json"
 fi
+
+cat "$summary_file_md" >> "$step_summary_file"
